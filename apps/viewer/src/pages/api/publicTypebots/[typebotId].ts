54a5e554de5e428e4ec018f579bee488b657b937
Det P <1250781+ark1@users.noreply.github.com>
2024-05-05 14:08:51 +0100

    Update [typebotId].ts

--- apps/viewer/src/pages/api/publicTypebots/[typebotId].ts
+++ apps/viewer/src/pages/api/publicTypebots/[typebotId].ts
@@ -3,11 +3,6 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import Cors from 'cors'
 import { initMiddleware, methodNotAllowed, notFound } from '@typebot.io/lib/api'
 
-const corsOptions = {
-  origin: 'https://typebotbuilder-oskptm5ol-ark1s-projects.vercel.app', // Allow only this origin
-  methods: ['GET'], // Only allow GET requests
-  optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
-}
 const cors = initMiddleware(Cors())
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
